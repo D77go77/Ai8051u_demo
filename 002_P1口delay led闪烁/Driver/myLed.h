e2b41d571b5c500da814ee5a30060db66a67f6e5
Z1R343L <19816013818@163.com>
2024-12-29 12:09:43 +0800

    chore: 添加LED闪烁驱动代码

---
+++ 002_P1口delay led闪烁/Driver/myLed.h
@@ -0,0 +1,14 @@
+#ifndef _MYLED_H
+#define _MYLED_H
+
+
+#include "common.h"
+
+
+
+void led_loop(void);
+
+
+#endif 
+
+
---
+++ 002_P1口delay led闪烁/User/common.h
@@ -0,0 +1,20 @@
+#ifndef _ALL_H
+#define _ALL_H
+
+#include <AI8051U.H>
+#include "stdio.h"
+#include "intrins.h"
+
+typedef 	unsigned char	u8;
+typedef 	unsigned int	u16;
+typedef 	unsigned long	u32;
+
+#define MAIN_Fosc        40000000UL//40Mhz
+
+#include "myDelay.h"
+#include "myLed.h"
+
+
+#endif 
+
+
